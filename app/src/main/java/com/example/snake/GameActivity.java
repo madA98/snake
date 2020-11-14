3e708adb08360d4e1b4df8372f9da3816364da0f
madA98 <adam.sarek@centrum.cz>
2020-11-15 00:26:57 +0100

    Added game objects (Snake, Apple), canvas drawing
    Added game objects Snake and Apple which use body Blocks to identify their position around map.
    At the beginning snake and apple spawn at random position and then snake moves 5x a second over the canvas and if it hits an apple, new apple spawns.
    So far there is not implement maneuvering, so snake is going in direction given at spawn.
    Game draws current state of game on the Canvas.
    Main menu now has own Snake logo and in highscores there is also prepared UI for later use.

--- app/src/main/java/com/example/snake/GameActivity.java
+++ app/src/main/java/com/example/snake/GameActivity.java
@@ -1,6 +1,7 @@
 package com.example.snake;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -23,8 +24,20 @@ public class GameActivity extends AppCompatActivity {
     private int screenWidth;
     private int screenHeight;
 
+    // Game sizes
+    private final int blockMargin = 10;
+    private final int gameTopBarHeight = 80;
+    private final int gameBottomBarHeight = 240;
+    private final int gameWidth = 20;
+    private int gameHeight;
+    private int gameTopMargin;
+    private int gameLeftMargin;
+    private int blockSize;
+
     // Game
     private String playerName;
+    private Apple apple;
+    private Snake snake;
 
     private Thread playThread = null;
     private volatile boolean playing = false;
@@ -37,8 +50,6 @@ public class GameActivity extends AppCompatActivity {
         // Load
         sp = getSharedPreferences("Game", Context.MODE_PRIVATE);
         playerName = sp.getString("playerName", null);
-
-        System.out.println("Player: " + playerName);
 
         gameView = new GameView(this);
         setContentView(gameView);
@@ -56,6 +67,12 @@ public class GameActivity extends AppCompatActivity {
         playing = false;
     }
 
+    private void openHighscores() {
+        Intent highscoresIntent = new Intent(GameActivity.this, HighscoresActivity.class);
+        startActivity(highscoresIntent);
+        finish();
+    }
+
     private void mute() {
         muted = true;
     }
@@ -65,7 +82,14 @@ public class GameActivity extends AppCompatActivity {
     }
 
     private void update() {
+        snake.move();
 
+        Block appleBlock = apple.getBlock();
+        Block[] snakeBlocks = snake.getBlocks();
+        if(snakeBlocks[0].getX() == appleBlock.getX() && snakeBlocks[0].getY() == appleBlock.getY()) {
+            snake.eat();
+            apple = new Apple(gameWidth, gameHeight, snakeBlocks);
+        }
     }
 
     private void redraw() {
@@ -86,6 +110,18 @@ public class GameActivity extends AppCompatActivity {
             if(hasWindowFocus) {
                 screenWidth = gameView.getMeasuredWidth();
                 screenHeight = gameView.getMeasuredHeight();
+
+                // Calculate blockSize and gameHeight
+                blockSize = (screenWidth - blockMargin * (gameWidth + 1)) / gameWidth;
+                gameLeftMargin = (screenWidth - (gameWidth * (blockSize + blockMargin) + blockMargin)) / 2;
+                gameHeight = (screenHeight - gameTopBarHeight - gameBottomBarHeight - blockMargin) / (blockSize + blockMargin);
+                gameTopMargin = gameTopBarHeight + (screenHeight - gameTopBarHeight - gameBottomBarHeight - (gameHeight * (blockSize + blockMargin) + blockMargin)) / 2;
+
+                // Spawn objects
+                snake = new Snake(gameWidth, gameHeight);
+                apple = new Apple(gameWidth, gameHeight, snake.getBlocks());
+
+                setWillNotDraw(false);
             }
         }
 
@@ -136,6 +172,32 @@ public class GameActivity extends AppCompatActivity {
             // Background
             paint.setColor(Color.BLACK);
             canvas.drawPaint(paint);
+
+            // Grid
+            paint.setColor(Color.argb(255, 34, 34, 34));
+            for(int i = 0; i < gameWidth; i++) {
+                for(int j = 0; j < gameHeight; j++) {
+                    x = gameLeftMargin + blockMargin + (blockSize + blockMargin) * i;
+                    y = gameTopMargin + blockMargin + (blockSize + blockMargin) * j;
+                    canvas.drawRoundRect(x, y, x + blockSize, y + blockSize, blockMargin, blockMargin, paint);
+                }
+            }
+
+            // Apple
+            paint.setColor(Color.argb(255, 34, 221, 34));
+            Block appleBlock = apple.getBlock();
+            x = gameLeftMargin + blockMargin + (blockSize + blockMargin) * appleBlock.getX();
+            y = gameTopMargin + blockMargin + (blockSize + blockMargin) * appleBlock.getY();
+            canvas.drawRoundRect(x, y, x + blockSize, y + blockSize, blockMargin, blockMargin, paint);
+
+            // Snake
+            Block[] snakeBlocks = snake.getBlocks();
+            for(int i = 0; i < snakeBlocks.length; i++) {
+                paint.setColor(Color.argb(127 + 128 / snakeBlocks.length * (snakeBlocks.length - i), 255, 255, 255));
+                x = gameLeftMargin + blockMargin + (blockSize + blockMargin) * snakeBlocks[i].getX();
+                y = gameTopMargin + blockMargin + (blockSize + blockMargin) * snakeBlocks[i].getY();
+                canvas.drawRoundRect(x, y, x + blockSize, y + blockSize, blockMargin, blockMargin, paint);
+            }
         }
     }
 }
